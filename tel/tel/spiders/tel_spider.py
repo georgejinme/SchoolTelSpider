461f4e43bd6fd5cd0a09c425b8436cd019909cb6
gougoumemeda <a516550257@qq.com>
2015-08-25 16:26:38 +0800

    优化数据显示格式

--- tel/tel/spiders/tel_spider.py
+++ tel/tel/spiders/tel_spider.py
@@ -4,7 +4,11 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 
 class telSpider(CrawlSpider):
-    schoolDic = {"SJTU": ["sjtu.edu.cn", "http://www.sjtu.edu.cn"]}
+
+    school = ""
+    schoolDic = {
+      "SJTU": ["sjtu.edu.cn", "http://www.sjtu.edu.cn", '(5474\d+)|(3420\d+)']
+    }
 
     name = "tel"
     allowed_domains = []
@@ -13,17 +17,17 @@ class telSpider(CrawlSpider):
 
     def __init__(self, school=None, *args, **kwargs):
       super(telSpider, self).__init__(*args, **kwargs)
-
-      self.allowed_domains = [self.schoolDic[school][0]]
+      self.school = school
+      self.allowed_domains = [self.schoolDic[self.school][0]]
       self.start_urls = [
-        self.schoolDic[school][1]
+        self.schoolDic[self.school][1]
       ]
       
 
 
     def parse_item(self, response):
       for sel in response.xpath('//*/text()'):
-        tel = sel.re('(5474\d+)|(3420\d+)')
+        tel = sel.re(self.schoolDic[self.school][2])
         if tel != []:
           item = TelItem()
           item['name'] = response.xpath('//title/text()').extract()
